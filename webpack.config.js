3dcb41006f2c58f3780c4221392c76bd7f63daf4
Kenni Silverio <ksilverio31@gmail.com>
2018-10-31 09:24:35 -0400

    last commit outside proxy making sure it's fully updated

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ var DIST_DIR = path.join(__dirname, '/react-client/dist');
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
   output: {
-    filename: 'bundle.js',
+    filename: 'Review.bundle.js',
     path: DIST_DIR
   },
   module : {
