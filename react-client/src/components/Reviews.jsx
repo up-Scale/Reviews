b855001e2745f368768c0d44cca1d971b3cc04a4
Kenni Silverio <ksilverio31@gmail.com>
2018-11-03 13:05:57 -0400

    more quick fixes

--- react-client/src/components/Reviews.jsx
+++ react-client/src/components/Reviews.jsx
@@ -4,16 +4,16 @@ import Review from './Review.jsx';
 const Reviews = (props) => {
   const reviewInfo = {
     // float: 'left',
-    width: '800px',
-    marginLeft: '20px',
-    marginRight: '20px',
-    paddingBottom: '15px',
-    color: '#5b6a69',
+    // width: '800px',
+    // marginLeft: '20px',
+    // marginRight: '20px',
+    // paddingBottom: '15px',
+    // color: '#5b6a69',
     // position: 'relative',
   }
 
   return (
-    <div className='container' style={reviewInfo}>
+    <div className='container'>
       <h4> Reviews </h4>
       There are { props.reviews.length } reviews.
       <div>
